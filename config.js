abc47e684a062e3fbe66edd00db57165636cf096
Naveed123412 <160267489+Naveed123412@users.noreply.github.com>
2024-02-16 23:03:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923080033556', 'Naveed', true],
+  ['6281255369012', 'Akhtar', true], 
   [''] 
 ] //Number of owners
 
